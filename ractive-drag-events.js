e96466d5f59677446d0339868fb91376b53fa1a5
andyhall <andy@fenomas.com>
2015-11-20 14:40:41 +0900

    Add `ractive` as peer dependency, and work correctly with CommonJS usage

--- ractive-drag-events.js
+++ ractive-drag-events.js
@@ -4,33 +4,52 @@
  * 
 */
 
-var eventNames = ['dragstart', 'dragenter', 'dragover', 'dragleave', 'drop', 'dragend']
-var niceNames = ['start', 'enter', 'over', 'leave', 'drop', 'end']
 
-function mainHandler(node, fire, event) {
-	var type = niceNames[eventNames.indexOf(event.type)]
-	fire({
-		node: node,
-		type: type,
-		target: this,
-		original: event
-	});
-}
+; (function (root, factory) {
+	
+	if (typeof module === "object" && module.exports) {
+		module.exports = factory(require('ractive'))
+	} else if (root.Ractive) {
+		factory(root.Ractive)
+	} else {
+		throw new Error("couldn't find Ractive module to modify")
+	}
+	
+} (this, function (Ractive) {
 
-Ractive.events.dragndrop = function (node, fire) {
+	var eventNames = ['dragstart', 'dragenter', 'dragover', 'dragleave', 'drop', 'dragend']
+	var niceNames = ['start', 'enter', 'over', 'leave', 'drop', 'end']
 
-	var boundHandler = mainHandler.bind(null, node, fire)
-
-	for (var i = 0; i < eventNames.length; i++) {
-		node.addEventListener(eventNames[i], boundHandler)
+	function mainHandler(node, fire, event) {
+		var type = niceNames[eventNames.indexOf(event.type)]
+		fire({
+			node: node,
+			type: type,
+			target: this,
+			original: event
+		});
 	}
 
-	return {
-		teardown: function () {
-			for (var i = 0; i < eventNames.length; i++) {
-				node.removeEventListener(eventNames[i], boundHandler)
+	Ractive.events.dragndrop = function (node, fire) {
+
+		var boundHandler = mainHandler.bind(null, node, fire)
+
+		for (var i = 0; i < eventNames.length; i++) {
+			node.addEventListener(eventNames[i], boundHandler)
+		}
+
+		return {
+			teardown: function () {
+				for (var i = 0; i < eventNames.length; i++) {
+					node.removeEventListener(eventNames[i], boundHandler)
+				}
 			}
 		}
+
 	}
 
-}
+}));
+
+
+
+
